4afbc643db15f0e18ce283d073afdc06b0152755
KostasA <konanast@gmail.com>
2016-06-25 12:14:49 +0200

    add second thread

--- thread.c
+++ thread.c
@@ -5,27 +5,38 @@
 #include <pthread.h>
 
 void *thread_function(void *arg);
+void *thread_function2(void *arg);
 char message[] = "Hello World";
 
 int main()
 {
 	int res;
-	pthread_t a_thread;
+	pthread_t a_thread,b_thread;
 	void *thread_result;
 
-	res = pthread_create(&a_thread,NULL,thread_function,(void*)message);
+	res = pthread_create(&a_thread,NULL,thread_function,(void *)message);
 	if(res!=0)
 	{
 		perror("Thread creation failed");
 		exit(EXIT_FAILURE);
 	}
-	printf("Waiting for thread to finish...\n");
+	res = pthread_create(&b_thread,NULL,thread_function2,(void *)message);
+	if(res!=0)
+	{
+		perror("Thread creation failed");
+		exit(EXIT_FAILURE);
+	}
+	printf("Waiting for 1st thread to finish...\n");
 	res = pthread_join(a_thread,&thread_result);
 	if(res!=0)
 	{
 		perror("Thread join failed");
 		exit(EXIT_FAILURE);
 	}
+	printf("Thread joined, it returned %s\n",(char *)thread_result);
+	printf("Message is now %s\n",message);
+	printf("Waiting for 2nd thread to finish...\n");
+	res = pthread_join(b_thread,&thread_result);
 	printf("Thread joined, it returned %s\n",(char *)thread_result);
 	printf("Message is now %s\n",message);
 	exit(EXIT_SUCCESS);
@@ -38,3 +49,12 @@ void *thread_function(void *arg)
 	strcpy(message,"Bye!");
 	pthread_exit("Thank you for the CPU time");
 }
+
+void *thread_function2(void *arg)
+{
+	printf("thread_function2 is running.\n");
+	sleep(10);
+	strcpy(message,"Bye from 2nd thread\n");
+	pthread_exit("Thank you for the CPU time also\n");
+}
+
