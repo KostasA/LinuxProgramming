dd66a821f1861fbf25f1762f93d3a00941196400
KostasA <konanast@gmail.com>
2016-06-26 17:22:13 +0200

    simultaneous execution

--- thread.c
+++ thread.c
@@ -4,9 +4,9 @@
 #include <string.h>
 #include <pthread.h>
 
-void *thread_function(void *arg);
-void *thread_function2(void *arg);
+void *thread_function();
 char message[] = "Hello World";
+int run_now = 1; //1 when the main function is executing and 2 when the new thread is executing
 
 int main()
 {
@@ -14,47 +14,50 @@ int main()
 	pthread_t a_thread,b_thread;
 	void *thread_result;
 
-	res = pthread_create(&a_thread,NULL,thread_function,(void *)message);
+	res = pthread_create(&a_thread,NULL,thread_function,NULL);
 	if(res!=0)
 	{
 		perror("Thread creation failed");
 		exit(EXIT_FAILURE);
 	}
-	res = pthread_create(&b_thread,NULL,thread_function2,(void *)message);
-	if(res!=0)
+	int print_count1=0;
+	while(print_count1++ < 20)
 	{
-		perror("Thread creation failed");
-		exit(EXIT_FAILURE);
+		if(run_now == 1)
+		{
+			printf("1");
+			run_now = 2;
+		}
+		else
+		{
+			sleep(1);
+		}
+		
 	}
-	printf("Waiting for 1st thread to finish...\n");
+	printf("Waiting for thread to finish...\n");
 	res = pthread_join(a_thread,&thread_result);
 	if(res!=0)
 	{
 		perror("Thread join failed");
 		exit(EXIT_FAILURE);
 	}
-	printf("Thread joined, it returned %s\n",(char *)thread_result);
-	printf("Message is now %s\n",message);
-	printf("Waiting for 2nd thread to finish...\n");
-	res = pthread_join(b_thread,&thread_result);
-	printf("Thread joined, it returned %s\n",(char *)thread_result);
-	printf("Message is now %s\n",message);
 	exit(EXIT_SUCCESS);
 }
 
-void *thread_function(void *arg)
-{
-	printf("thread_function is running.Argument was %s\n",(char *) arg);
-	sleep(3);
-	strcpy(message,"Bye!");
-	pthread_exit("Thank you for the CPU time");
+void *thread_function()
+{	
+	int print_count2 = 0;
+	while(print_count2++ < 20)
+	{
+		if(run_now == 2)
+		{
+			printf("2");
+			run_now = 1;
+		}
+		else
+		{
+			sleep(1);
+		}
+	}
+	printf("thread_function is running.");
 }
-
-void *thread_function2(void *arg)
-{
-	printf("thread_function2 is running.\n");
-	sleep(10);
-	strcpy(message,"Bye from 2nd thread\n");
-	pthread_exit("Thank you for the CPU time also\n");
-}
-
